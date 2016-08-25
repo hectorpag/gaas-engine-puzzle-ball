8097fee2a557be9b5870aa52950ddcf2d90b9469
vpate <vpate@DESKTOP-UVS2Q76>
2016-08-25 11:42:10 +1000

    Disable Home page

--- PaperToss.Web/Controllers/HomeController.cs
+++ PaperToss.Web/Controllers/HomeController.cs
@@ -9,12 +9,12 @@ namespace PaperToss.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IGameService _gameService;
-      
+
 
         public HomeController(IGameService gameService)
         {
             _gameService = gameService;
-        
+
         }
         // GET: Home
         public ActionResult Index(string campaignKey, string panelId, string consumerId)
@@ -28,18 +28,23 @@ namespace PaperToss.Web.Controllers
 
             var gameViewModel = _gameService.Load(gaasInfoViewModel);
 
-            if (gameViewModel.Config.ShowMenu)
-            {
-                return View(gameViewModel);
-            }
-            else if (gameViewModel.Config.ShowResult)
-            {
-                return RedirectToRoute("result", new { campaignKey = gaasInfoViewModel.CampaignKey, panelId = gaasInfoViewModel.PanelId, consumerId = gaasInfoViewModel.ConsumerId });
-            }
-            else
-            {
-                return RedirectToRoute("game", new { campaignKey = gaasInfoViewModel.CampaignKey, panelId = gaasInfoViewModel.PanelId, consumerId = gaasInfoViewModel.ConsumerId });
-            }
+            //Add another field in config, to state if panel has home/result page. If not then use it in combination with ShowResult/ShowMenu, Or something that can identify if game has a data capture in between home,levels & results
+
+            return RedirectToRoute("game", new { campaignKey = gaasInfoViewModel.CampaignKey, panelId = gaasInfoViewModel.PanelId, consumerId = gaasInfoViewModel.ConsumerId });
+           
+
+            //if (gameViewModel.Config.ShowMenu)
+            //{
+            //    return View(gameViewModel);
+            //}
+            //else if (gameViewModel.Config.ShowResult)
+            //{
+            //    return RedirectToRoute("result", new { campaignKey = gaasInfoViewModel.CampaignKey, panelId = gaasInfoViewModel.PanelId, consumerId = gaasInfoViewModel.ConsumerId });
+            //}
+            //else
+            //{
+            //    return RedirectToRoute("game", new { campaignKey = gaasInfoViewModel.CampaignKey, panelId = gaasInfoViewModel.PanelId, consumerId = gaasInfoViewModel.ConsumerId });
+            //}
         }
     }
 }
