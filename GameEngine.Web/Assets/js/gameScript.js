fd2b821fbb09f1fa0c1249ee48bfc8dad2807d03
jacobfife <jacobfife@JACOBFIFE0D8F>
2017-05-04 14:07:28 +1000

    check status of variables

--- GameEngine.Web/Assets/js/gameScript.js
+++ GameEngine.Web/Assets/js/gameScript.js
@@ -20,6 +20,11 @@ var posValues = [70, 270, 470];
 
 var maxNumberOfZombies = 10;
 
+var somethingWrong = false;
+var whatsWrong = '';
+
+var problemArray = [0,0,0,0,0,0,0,0,0,0,0]
+
 function updateTacklesMade() {
     tacklesMade++;
     tacklesMadeInLevel++;
@@ -39,14 +44,20 @@ function startGame() {
 function checkEndGame() {
     if (missedZombies >= 3) {
         endGame();
+        console.log(whatsWrong);
     } else if (tacklesMadeInLevel + missedZombiesInLevel >= maxNumberOfZombies) {
         showPopup();
+        console.log(whatsWrong);
     }
 }
 
 function showPopup() {
     clearInterval(loop);
     $('#questionDialog').show();
+}
+
+function endGame() {
+    clearInterval(loop);
 }
 
 function resetGame() {
@@ -63,8 +74,61 @@ function resetGame() {
     startGame();
 }
 
-function endGame() {
-    clearInterval(loop);
+function checkHealth() {
+    if (missedZombies > 3 || missedZombiesInLevel > 3) {
+        saveProblem(' Missed too many zombies.', 0);
+        missedZombies = 3;
+        missedZombiesInLevel = 3;
+    }
+    if (missedZombies < 0 || missedZombiesInLevel < 0) {
+        saveProblem(' Incorrect missed tackles.', 1);
+        missedZombies = 0;
+    }
+    if (tacklesMadeInLevel > levelNumber * 10) {
+        saveProblem(' Made too many tackles.', 2);
+        tacklesMadeInLevel = maxNumberOfZombies;
+    }
+    if (timeToNextSpawn > fps) {
+        saveProblem(' Zombies spawning too slow.', 3);
+        timeTakenToSpawn = fps;
+    }
+    if (outOfBounds !== -100) {
+        saveProblem(' Game area changed.', 4);
+        outOfBounds = -100;
+    }
+    if (noOfZombies > maxNumberOfZombies) {
+        saveProblem(' Too many zombies.', 5);
+        noOfZombies = maxNumberOfZombies;
+    }
+    if (noOfZombies < 0) {
+        saveProblem(' Incorrect number of zombies.', 6);
+        noOfZombies = 0;
+    }
+    if (levelNumber * 10 !== maxNumberOfZombies) {
+        saveProblem(' Incorrect maximum number of zombies.', 7);
+    }
+    if (timeTakenToSpawn > timeToNextSpawn) {
+        saveProblem(' Zombies spawning too slow.', 8);
+        timeTakenToSpawn = timeToNextSpawn;
+    }
+    for (i = 0; i < zombies.length; i++) {
+        if (zombies[i].speed < -500) {
+            saveProblem(' Zombies spawning too fast.', 9);
+            zombies[i].speed = -500;
+        }
+        if (-200 < zombies[i].speed) {
+            saveProblem(' Zombies spawning too slow.', 10);
+            zombies[i].speed = -200;
+        }
+    }
+}
+
+function saveProblem(_message, _id) {
+    if (problemArray[_id] === 0) {
+        whatsWrong += _message;
+        problemArray[_id] = 1;
+    }
+    somethingWrong = true;
 }
 
 function mainLoop() {
@@ -85,6 +149,7 @@ function mainLoop() {
     destroyZombies(); 
     
     checkEndGame();
+    checkHealth();
 }
 
 function checkKeyPress(e) {
