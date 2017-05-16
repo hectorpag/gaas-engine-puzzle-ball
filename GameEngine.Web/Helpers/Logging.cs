47eb379759a0ba0e973603ee76c1ff8cbe31f6a2
Steve Shearn <steve@syntronian.com>
2017-05-16 10:23:23 +1000

    catch gaas api access and trace error and return gracefully

--- GameEngine.Web/Helpers/Logging.cs
+++ GameEngine.Web/Helpers/Logging.cs
@@ -10,12 +10,14 @@ namespace GameEngine.Web.Helpers
         public static void Error(Exception ex, string messageTemplate = null)
         {
             //var telem = new TelemetryClient(); telem.TrackException(ex); telem.Flush();
+            System.Diagnostics.Trace.TraceError(ex.Message, ex.StackTrace);
             Serilog.Log.Error(ex, messageTemplate ?? ex.Message);
         }
 
         public static void Info(string messageTemplate, object propertyValue = null)
         {
 #if DEBUG
+            System.Diagnostics.Trace.TraceInformation(messageTemplate, propertyValue);
             Serilog.Log.Information(messageTemplate, propertyValue);
 #endif
         }
